ebb68aa0adfd6722705e98dbf923220d87a66a60
Bruce LANE <bruce@batchass.fr>
2016-10-27 22:57:30 +0200

    component shader.js

--- index.android.js
+++ index.android.js
@@ -11,43 +11,18 @@ import {
   Text,
   View
 } from 'react-native';
-
+const GL = require("gl-react-native");
+//const Shader = require('./src/shader.js');
+import Shader from './src/shader.js'
+const { Surface } = require("gl-react-native"); // in React Native context
 export default class VideodrommMobile extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
+        <Surface width={511} height={341}>
+          <Shader blue={0.5} />
+        </Surface>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
 
 AppRegistry.registerComponent('VideodrommMobile', () => VideodrommMobile);
