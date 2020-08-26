f82ce43edf35bd35e015abd867889a78f03dbf13
Bruce <bruce@batchass.fr>
2020-08-26 14:59:29 +0200

    androidx

--- src/pages/Support.tsx
+++ src/pages/Support.tsx
@@ -3,7 +3,8 @@ import React, { useState } from 'react';
 import { IonHeader, IonToolbar, IonTitle, IonContent, IonPage, IonButtons, IonMenuButton, IonRow, IonCol, IonButton, IonList, IonItem, IonLabel, IonText, IonTextarea, IonToast } from '@ionic/react';
 import './Login.scss';
 import { connect } from '../data/connect';
-import { Button,
+import {
+  Button,
   Toggle,
   Dial,
   Number,
@@ -18,15 +19,16 @@ import { Button,
   TextButton,
   Tilt,
   Pan,
-  Pan2D } from 'react-nexusui';
-  const dialColors = ['#a70', '#f00', '#0f0', '#00f', '#aaa', '#f00', '#0f0', '#00f', '#370', '#ff0', '#fff', '#a0f', '#a70', '#270', '#fff', '#a0f', '#370', '#ff0'];
-  const dialNames = ['time', 'red', 'green', 'blue', 'alpha', 'red*', 'green*', 'blue*', 'sobel', 'badtv', 'steps', 'ratio', 'zoom', 'audio*', 'expo', 'pixel8', 'trixel', 'chroma'];
+  Pan2D
+} from 'react-nexusui';
+const dialColors = ['#a70', '#f00', '#0f0', '#00f', '#aaa', '#f00', '#0f0', '#00f', '#370', '#ff0', '#fff', '#a0f', '#a70', '#270', '#fff', '#a0f', '#370', '#ff0'];
+const dialNames = ['time', 'red', 'green', 'blue', 'alpha', 'red*', 'green*', 'blue*', 'sobel', 'badtv', 'steps', 'ratio', 'zoom', 'audio*', 'expo', 'pixel8', 'trixel', 'chroma'];
 
-  declare global {
-    interface Window { socket: any; ws: any; }
+declare global {
+  interface Window { socket: any; ws: any; }
 }
 function initWs() {
-  window.ws = (function (uri:string) {
+  window.ws = (function (uri: string) {
     console.log('ws init')
     window.ws = new WebSocket(uri);
     /*window.ws.onmessage = function (evt) {
@@ -53,57 +55,57 @@ function initWs() {
   });
 
   //window.socket = new ws('ws://127.0.0.1:8088');
-  window.socket = new WebSocket('ws://127.0.0.1:8088');
+  window.socket = new WebSocket('ws://192.168.0.24:8088');
 }
 initWs();
-  const emitToSocket = (value:number, index:number) => {
-    console.log(value);
-    /*
-    CONNECTING	0
-    OPEN	1
-    CLOSING	2
-    CLOSED	3
-    */
-    if (window.socket && window.socket.readyState === 1) {
-      window.socket.send('{"params" :[{"name" : ' + index + ',"value" :' + value + '}]}');
-      //console.log(`emitToSocket readyState ${window.ws.readyState}, val: ${value}, idx: ${index} `);
-      console.log(`emitToSocket readyStateval: ${value}, idx: ${index} `);
+const emitToSocket = (value: number, index: number) => {
+  console.log(value);
+  /*
+  CONNECTING	0
+  OPEN	1
+  CLOSING	2
+  CLOSED	3
+  */
+  if (window.socket && window.socket.readyState === 1) {
+    window.socket.send('{"params" :[{"name" : ' + index + ',"value" :' + value + '}]}');
+    //console.log(`emitToSocket readyState ${window.ws.readyState}, val: ${value}, idx: ${index} `);
+    console.log(`emitToSocket readyStateval: ${value}, idx: ${index} `);
 
-    } else {
-      //console.log(`not ready, state ${window.ws.readyState}`);
-      console.log(`not ready`);
-    }
-
-  };
-
-
-  function TitleAndChildren({ children, title }) {
-    return (
-      <div style={{ margin: 10 }}>
-        <h2 className={"subtitle"} style={{ textAlign: "center" }}>{title}</h2>
-        {children}
-      </div>
-    );
+  } else {
+    //console.log(`not ready, state ${window.ws.readyState}`);
+    console.log(`not ready`);
   }
 
-  const mouseChange = (pos) => {
-    console.log(`mouseChange ${pos.x} ${pos.y} `);
-    if (window.socket && window.ws.readyState === 1) {
-      window.socket.send('{"params" :[{"name" : 42,"value" :' + pos.x + '},{"name" : 43,"value" :' + pos.y + '}]}');
-    }
-  }
-  const xFadeChange = (val:number) => {
-    console.log(`mouseChange ${val} `);
-    emitToSocket(val, 18);
-  }
-  const mixChange = (valuesArray) => {
-    console.log(`mixChange ${JSON.stringify(valuesArray)} } `);// 31 to 39
-    valuesArray.map((item, index) => {
+};
 
-      console.log(`mixChange ${item} ${index} `);
-      emitToSocket(item, 31 + index);
-    });
+
+function TitleAndChildren({ children, title }) {
+  return (
+    <div style={{ margin: 10 }}>
+      <h2 className={"subtitle"} style={{ textAlign: "center" }}>{title}</h2>
+      {children}
+    </div>
+  );
+}
+
+const mouseChange = (pos) => {
+  console.log(`mouseChange ${pos.x} ${pos.y} `);
+  if (window.socket && window.ws.readyState === 1) {
+    window.socket.send('{"params" :[{"name" : 42,"value" :' + pos.x + '},{"name" : 43,"value" :' + pos.y + '}]}');
   }
+}
+const xFadeChange = (val: number) => {
+  console.log(`mouseChange ${val} `);
+  emitToSocket(val, 18);
+}
+const mixChange = (valuesArray) => {
+  console.log(`mixChange ${JSON.stringify(valuesArray)} } `);// 31 to 39
+  valuesArray.map((item, index) => {
+
+    console.log(`mixChange ${item} ${index} `);
+    emitToSocket(item, 31 + index);
+  });
+}
 
 interface OwnProps { }
 
@@ -143,27 +145,96 @@ const Support: React.FC<SupportProps> = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent>
-
+      <IonRow>
+            <IonCol>
+            <Dial
+              interaction={"radial"}
+              key={1}
+              onReady={dialRef => {
+                dialRefs.current.push(dialRef);
+                dialRef.colorize("accent", dialColors[1]);
+              }}
+              onChange={value => {
+                console.log(value)
+                emitToSocket(value, 1);
+              }}
+              value={1}
+              min={0}
+              max={1}
+            />
+          </IonCol>
+           <IonCol>
+              <Dial
+                interaction={"radial"}
+                key={2}
+                onReady={dialRef => {
+                  dialRefs.current.push(dialRef);
+                  dialRef.colorize("accent", dialColors[2]);
+                }}
+                onChange={value => {
+                  console.log(value)
+                  emitToSocket(value, 2);
+                }}
+                value={1}
+                min={0}
+                max={1}
+              />
+            </IonCol>
+            <IonCol>
+              <Dial
+                interaction={"radial"}
+                key={3}
+                onReady={dialRef => {
+                  dialRefs.current.push(dialRef);
+                  dialRef.colorize("accent", dialColors[3]);
+                }}
+                onChange={value => {
+                  console.log(value)
+                  emitToSocket(value, 3);
+                }}
+                value={1}
+                min={0}
+                max={1}
+              />
+            </IonCol>
+             <IonCol>
+              <Dial
+                interaction={"radial"}
+                key={4}
+                onReady={dialRef => {
+                  dialRefs.current.push(dialRef);
+                  dialRef.colorize("accent", dialColors[4]);
+                }}
+                onChange={value => {
+                  console.log(value)
+                  emitToSocket(value, 4);
+                }}
+                value={1}
+                min={0}
+                max={1}
+              />
+            </IonCol>
+          </IonRow>
         <div className="login-logo">
-        <TextButton text="Click me" onChange={console.log} />
+          <TextButton text="Click me" onChange={console.log} />
         </div>
 
         <div className="login-logo">
-        <Dial
-          interaction={"radial"}
-          key={0}
-          onReady={dialRef => {
-            dialRefs.current.push(dialRef);
-            dialRef.colorize("accent", dialColors[0]);
-          }}
-          onChange={value => {
-            console.log(value)
-            emitToSocket(value, 0);
-          }}
-          value={1}
-          min={0}
-          max={1}
-        />
+          <Dial
+            interaction={"radial"}
+            key={0}
+            onReady={dialRef => {
+              dialRefs.current.push(dialRef);
+              dialRef.colorize("accent", dialColors[0]);
+            }}
+            onChange={value => {
+              console.log(value)
+              emitToSocket(value, 0);
+            }}
+            value={1}
+            min={0}
+            max={1}
+          />
 
 
         </div>
@@ -183,11 +254,11 @@ const Support: React.FC<SupportProps> = () => {
             </IonText>}
           </IonList>
 
+          
           <IonRow>
             <IonCol>
               <IonButton type="submit" expand="block">Submit</IonButton>
-            </IonCol>
-          </IonRow>
+            </IonCol></IonRow>
         </form>
 
       </IonContent>
