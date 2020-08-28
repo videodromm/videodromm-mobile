369e0f189ed1d1f11e271f54bc0dfec439ee4b2e
Bruce <bruce@batchass.fr>
2020-08-28 18:30:05 +0200

    wip

--- src/pages/Support.tsx
+++ src/pages/Support.tsx
@@ -21,40 +21,8 @@ import {
   Pan,
   Pan2D
 } from 'react-nexusui';
-const dialColors = ['#a70', '#f00', '#0f0', '#00f', '#aaa', '#f00', '#0f0', '#00f', '#370', '#ff0', '#fff', '#a0f', '#a70', '#270', '#fff', '#a0f', '#370', '#ff0'];
-const dialNames = ['time', 'red', 'green', 'blue', 'alpha', 'red*', 'green*', 'blue*', 'sobel', 'badtv', 'steps', 'ratio', 'zoom', 'audio*', 'expo', 'pixel8', 'trixel', 'chroma'];
-
-/**/
-const emitToSocket = (value: number, index: number) => {
-  console.log(value);
-  /*
-  CONNECTING	0
-  OPEN	1
-  CLOSING	2
-  CLOSED	3
-  */
-  if (window.socket && window.socket.readyState === 1) {
-    window.socket.send('{"params" :[{"name" : ' + index + ',"value" :' + value + '}]}');
-    //console.log(`emitToSocket readyState ${window.ws.readyState}, val: ${value}, idx: ${index} `);
-    console.log(`emitToSocket readyStateval: ${value}, idx: ${index} `);
-
-  } else {
-    //console.log(`not ready, state ${window.ws.readyState}`);
-    console.log(`not ready`);
-  }
-
-};
 
 /*
-function TitleAndChildren({ children, title }) {
-  return (
-    <div style={{ margin: 10 }}>
-      <h2 className={"subtitle"} style={{ textAlign: "center" }}>{title}</h2>
-      {children}
-    </div>
-  );
-}
-
 const mouseChange = (pos) => {
   console.log(`mouseChange ${pos.x} ${pos.y} `);
   if (window.socket && window.ws.readyState === 1) {
@@ -82,12 +50,40 @@ interface SupportProps extends OwnProps, DispatchProps { }
 
 const Support: React.FC<SupportProps> = () => {
 
-  const [message, setMessage] = useState('ws://51.210.25.82:8088');
-  const [formSubmitted, setFormSubmitted] = useState(false);
-  const [messageError, setMessageError] = useState(false);
-
+  const dialColors = ['#a70', '#f00', '#0f0', '#00f', '#aaa', '#f00', '#0f0', '#00f', '#370', '#ff0', '#fff', '#a0f', '#a70', '#270', '#fff', '#a0f', '#370', '#ff0'];
+  const dialNames = ['time', 'red', 'green', 'blue', 'alpha', 'red*', 'green*', 'blue*', 'sobel', 'badtv', 'steps', 'ratio', 'zoom', 'audio*', 'expo', 'pixel8', 'trixel', 'chroma'];
   let dials = [] as React.ReactElement[];
   let dialRefs = React.useRef([] as any[]);
+ 
+  const emitToSocket = (value: number, index: number) => {
+    /*
+    CONNECTING	0
+    OPEN	1
+    CLOSING	2
+    CLOSED	3
+    */
+    if (window.socket && window.socket.readyState === 1) {
+      window.socket.send('{"params" :[{"name" : ' + index + ',"value" :' + value + '}]}');
+      console.log(`emitToSocket readyState val: ${value}, idx: ${index} `);
+    } else {
+      console.log(`not ready val: ${value}, idx: ${index} `);
+      /* ko if (dialRefs[1]) {
+        console.log(`emitToSocket dialRefs[0]: ${dialRefs[0]},  ${JSON.stringify(dialRefs[0])} `);
+        dialRefs[1].value = 0.5;
+      }*/
+    }
+  
+  };
+  
+  
+  function TitleAndChildren({ children, title }) {
+    return (
+      <div style={{ margin: 10 }}>
+        <h2 className={"subtitle"} style={{ textAlign: "center" }}>{title}</h2>
+        {children}
+      </div>
+    );
+  }
 
   return (
     <IonPage id="support-page">
@@ -96,12 +92,12 @@ const Support: React.FC<SupportProps> = () => {
           <IonButtons slot="start">
             <IonMenuButton></IonMenuButton>
           </IonButtons>
-          <IonTitle>Videodromm Websocket Controller</IonTitle>
+          <IonTitle>Videodromm Controller</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent>
       <IonRow>
-            <IonCol>
+          <IonCol>
             <Dial
               interaction={"radial"}
               key={1}
@@ -110,7 +106,6 @@ const Support: React.FC<SupportProps> = () => {
                 dialRef.colorize("accent", dialColors[1]);
               }}
               onChange={value => {
-                console.log(value)
                 emitToSocket(value, 1);
               }}
               value={1}
@@ -127,7 +122,6 @@ const Support: React.FC<SupportProps> = () => {
                   dialRef.colorize("accent", dialColors[2]);
                 }}
                 onChange={value => {
-                  console.log(value)
                   emitToSocket(value, 2);
                 }}
                 value={1}
@@ -144,7 +138,6 @@ const Support: React.FC<SupportProps> = () => {
                   dialRef.colorize("accent", dialColors[3]);
                 }}
                 onChange={value => {
-                  console.log(value)
                   emitToSocket(value, 3);
                 }}
                 value={1}
@@ -161,7 +154,6 @@ const Support: React.FC<SupportProps> = () => {
                   dialRef.colorize("accent", dialColors[4]);
                 }}
                 onChange={value => {
-                  console.log(value)
                   emitToSocket(value, 4);
                 }}
                 value={1}
@@ -178,7 +170,6 @@ const Support: React.FC<SupportProps> = () => {
                   dialRef.colorize("accent", dialColors[5]);
                 }}
                 onChange={value => {
-                  console.log(value)
                   emitToSocket(value, 5);
                 }}
                 value={1}
@@ -195,7 +186,6 @@ const Support: React.FC<SupportProps> = () => {
                   dialRef.colorize("accent", dialColors[6]);
                 }}
                 onChange={value => {
-                  console.log(value)
                   emitToSocket(value, 6);
                 }}
                 value={1}
@@ -212,7 +202,6 @@ const Support: React.FC<SupportProps> = () => {
                   dialRef.colorize("accent", dialColors[7]);
                 }}
                 onChange={value => {
-                  console.log(value)
                   emitToSocket(value, 7);
                 }}
                 value={1}
@@ -229,7 +218,6 @@ const Support: React.FC<SupportProps> = () => {
                   dialRef.colorize("accent", dialColors[8]);
                 }}
                 onChange={value => {
-                  console.log(value)
                   emitToSocket(value, 8);
                 }}
                 value={1}
@@ -238,31 +226,7 @@ const Support: React.FC<SupportProps> = () => {
               />
             </IonCol>
           </IonRow>
-        {/* <div className="login-logo">
-          <TextButton text="Connect" onChange={console.log} />
-        </div>
-
-        <div className="login-logo">
-          <Dial
-            interaction={"radial"}
-            key={0}
-            onReady={dialRef => {
-              dialRefs.current.push(dialRef);
-              dialRef.colorize("accent", dialColors[0]);
-            }}
-            onChange={value => {
-              console.log(value)
-              emitToSocket(value, 0);
-            }}
-            value={1}
-            min={0}
-            max={1}
-          />
-
-
-        </div> */}
         
-
       </IonContent>
 
       
