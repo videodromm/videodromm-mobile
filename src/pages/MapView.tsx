267b6c0595ab86aa1e7ef18ee30d2c06551e54ea
BruceLane <bruce@batchass.fr>
2021-05-17 23:52:41 +0200

    gl-react v4 + gl-react-dom

--- src/pages/MapView.tsx
+++ src/pages/MapView.tsx
@@ -5,6 +5,8 @@ import { Location } from '../models/Location';
 import { connect } from '../data/connect';
 import * as selectors from '../data/selectors';
 import './MapView.scss';
+import { Shaders, Node, GLSL } from "gl-react";
+import { Surface } from "gl-react-dom";
 
 interface OwnProps { }
 
@@ -17,7 +19,20 @@ interface DispatchProps { }
 
 interface MapViewProps extends OwnProps, StateProps, DispatchProps { };
 
+
 const MapView: React.FC<MapViewProps> = ({ locations, mapCenter }) => {
+
+  const shaders = Shaders.create({
+    helloBlue: {
+      frag: GLSL`
+  precision highp float;
+  varying vec2 uv;
+  void main() {
+    gl_FragColor = vec4(uv.x, uv.y, 0.1, 1.0);
+  }`
+    }
+  });
+  const blue = 0.5;
   return (
   <IonPage id="map-view">
     <IonHeader>
@@ -30,7 +45,24 @@ const MapView: React.FC<MapViewProps> = ({ locations, mapCenter }) => {
     </IonHeader>
 
     <IonContent class="map-page">
-      <Map locations={locations} mapCenter={mapCenter} />
+      <p>MapView</p>
+      <Surface width={100} height={100} >
+        <Node
+          shader={shaders.helloBlue}
+
+        />
+      </Surface>
+ {/* onMouseMove={this.handleMouseMove}uniforms={{
+            shift: 1.6,
+            time: time / 1000,
+            speed: [1.0, 1.0],
+            resolution: [this.width, this.height]
+          }}
+      <Node
+      shader={shaders.helloBlue}
+
+    />
+      <Map locations={locations} mapCenter={mapCenter} /> */}
     </IonContent>
   </IonPage>
 )};
