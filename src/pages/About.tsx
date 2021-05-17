717b7dc48527aa02a090e80435976420e4419c3a
BruceLane <bruce@batchass.fr>
2021-05-18 00:27:53 +0200

    time animation

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,43 +1,19 @@
 import React, { useState } from 'react';
 import { IonHeader, IonToolbar, IonContent, IonPage, IonButtons, IonMenuButton, IonButton, IonIcon, IonDatetime, IonSelectOption, IonList, IonItem, IonLabel, IonSelect, IonPopover } from '@ionic/react';
 import './About.scss';
-import { ellipsisHorizontal, ellipsisVertical } from 'ionicons/icons';
-import AboutPopover from '../components/AboutPopover';
+import ShadertoyReact from 'shadertoy-react';
+
 
 interface AboutProps { }
 
 const About: React.FC<AboutProps> = () => {
-
-  const [showPopover, setShowPopover] = useState(false);
-  const [popoverEvent, setPopoverEvent] = useState();
-  const [location, setLocation] = useState<'madison' | 'austin' | 'chicago' | 'seattle'>('madison');
-  const [shadaDate, setShadaDate] = useState('2047-05-17T00:00:00-05:00');
-
-  const selectOptions = {
-    header: 'Select a Location'
-  };
-
-  const presentPopover = (e: React.MouseEvent) => {
-    setPopoverEvent(e.nativeEvent);
-    setShowPopover(true);
-  };
-
-  // momentjs would be a better way to do this https://momentjs.com/
-  function displayDate(date: string, format: string) {
-    const monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-
-    const d = new Date(date);
-    const year = d.getFullYear();
-
-    if (format === 'y') {
-      return year;
-    } else {
-      const month = monthNames[d.getMonth()];
-      const day = d.getDate();
-
-      return month + ' ' + day + ', ' + year;
-    }
+  const fragmentShader = `
+  void main(void) {
+     vec2 uv = gl_FragCoord.xy/iResolution.xy;
+     vec3 col = 0.5 + 0.5*cos(iTime+uv.xyx+vec3(0,2,4));
+     gl_FragColor = vec4(col ,1.0);
   }
+`;
 
   return (
     <IonPage id="about-page">
@@ -45,87 +21,23 @@ const About: React.FC<AboutProps> = () => {
         <IonHeader className="ion-no-border">
           <IonToolbar>
             <IonButtons slot="start">
-              <IonMenuButton></IonMenuButton>
+              <IonMenuButton>jjj</IonMenuButton>
             </IonButtons>
             <IonButtons slot="end">
-              <IonButton onClick={presentPopover}>
-                <IonIcon slot="icon-only" ios={ellipsisHorizontal} md={ellipsisVertical}></IonIcon>
+              <IonButton >
+                <IonIcon slot="icon-only" ></IonIcon>
               </IonButton>
             </IonButtons>
           </IonToolbar>
         </IonHeader>
-<canvas className="about-canvas" width="150" height="150"></canvas>
-        <div className="about-header">
-          {/* Instead of loading an image each time the select changes, use opacity to transition them */}
-          <div className="about-image madison" style={{'opacity': location === 'madison' ? '1' : undefined}}></div>
-          <div className="about-image austin" style={{'opacity': location === 'austin' ? '1' : undefined}}></div>
-          <div className="about-image chicago" style={{'opacity': location === 'chicago' ? '1' : undefined}}></div>
-          <div className="about-image seattle" style={{'opacity': location === 'seattle' ? '1' : undefined}}></div>
-        </div>
-        <div className="about-info">
-          <h3 className="ion-padding-top ion-padding-start">About</h3>
 
-          <p className="ion-padding-start ion-padding-end">
-             on { displayDate(shadaDate, 'mediumDate') }    </p>
 
-          <h3 className="ion-padding-top ion-padding-start">Details</h3>
 
-          <IonList lines="none">
-            <IonItem>
-              <IonLabel>
-                Location
-              </IonLabel>
-              <IonSelect value={location} interfaceOptions={selectOptions} onIonChange={(e) => setLocation(e.detail.value as any)}>
-                <IonSelectOption value="madison">Madison, WI</IonSelectOption>
-                <IonSelectOption value="austin">Austin, TX</IonSelectOption>
-                <IonSelectOption value="chicago">Chicago, IL</IonSelectOption>
-                <IonSelectOption value="seattle">Seattle, WA</IonSelectOption>
-              </IonSelect>
-            </IonItem>
-            <IonItem>
-              <IonLabel>
-                Date
-              </IonLabel>
-              <IonDatetime
-                displayFormat="MMM DD, YYYY"
-                max="2056"
-                value={shadaDate}
-                onIonChange={(e) => setShadaDate(e.detail.value as any)}>
-              </IonDatetime>
-            </IonItem>
-          </IonList>
+         <ShadertoyReact fs={fragmentShader}/>
 
-          <h3 className="ion-padding-top ion-padding-start">Internet</h3>
-
-          <IonList lines="none">
-            <IonItem>
-              <IonLabel>
-                Wifi network
-              </IonLabel>
-              <IonLabel className="ion-text-end">
-                ica{ displayDate(shadaDate, 'y') }
-              </IonLabel>
-            </IonItem>
-            <IonItem>
-              <IonLabel>
-               Port
-              </IonLabel>
-              <IonLabel className="ion-text-end">
-                makegoodthings
-              </IonLabel>
-            </IonItem>
-          </IonList>
-
-        </div>
       </IonContent>
 
-      <IonPopover
-        isOpen={showPopover}
-        event={popoverEvent}
-        onDidDismiss={() => setShowPopover(false)}
-      >
-        <AboutPopover dismiss={() => setShowPopover(false)} />
-      </IonPopover>
+
     </IonPage>
   );
 };
--- src/pages/MapView.tsx
+++ src/pages/MapView.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import Map from '../components/Map';
 import { IonHeader, IonToolbar, IonButtons, IonMenuButton, IonTitle, IonContent, IonPage } from '@ionic/react';
 import { Location } from '../models/Location';
@@ -30,9 +30,73 @@ const MapView: React.FC<MapViewProps> = ({ locations, mapCenter }) => {
   void main() {
     gl_FragColor = vec4(uv.x, uv.y, 0.1, 1.0);
   }`
+    },
+    smoke: {
+      frag: GLSL`
+        precision mediump float;
+
+        uniform vec2      resolution;
+        uniform float     time;
+        uniform float     alpha;
+        uniform vec2      speed;
+        uniform float     shift;
+
+        float rand(vec2 n) {
+          return fract(sin(dot(n, vec2(6.9898, 1.1414))) * 4.0);
+        }
+
+        float noise(vec2 n) {
+          const vec2 d = vec2(0.0, 1.0);
+          vec2 b = floor(n), f = smoothstep(vec2(0.0), vec2(1.0), fract(n));
+          return mix(mix(rand(b), rand(b + d.yx), f.x), mix(rand(b + d.xy), rand(b + d.yy), f.x), f.y);
+        }
+
+        float fbm(vec2 n) {
+          float total = 0.0, amplitude = 1.0;
+          for (int i = 0; i < 4; i++) {
+            total += noise(n) * amplitude;
+            n += n;
+            amplitude *= 0.5;
+          }
+          return total;
+        }
+
+        void main() {
+
+          const vec3 c1 = vec3(1.0/255.0, 0.0/255.0, 97.0/255.0);
+          const vec3 c2 = vec3(150.0/255.0, 0.0/255.0, 161.4/255.0);
+          const vec3 c3 = vec3(0.0, 0.0, 0.8);
+          const vec3 c4 = vec3(164.0/255.0, 1.0/255.0, 214.4/255.0);
+          const vec3 c5 = vec3(0.0);
+          const vec3 c6 = vec3(0.6);
+
+          vec2 p = gl_FragCoord.xy * 8.0 / resolution.xx;
+          float q = fbm(p - time * 0.5);
+          vec2 r = vec2(fbm(p + q + time * speed.x - p.x - p.y), fbm(p + q - time * speed.y));
+          vec3 c = mix(c1, c2, fbm(p + r)) + mix(c3, c4, r.x) - mix(c5, c6, r.y);
+          float grad = gl_FragCoord.x / resolution.x;
+          gl_FragColor = vec4(c * sin(shift * gl_FragCoord.y / resolution.y), 1.0);
+          gl_FragColor.xyz *= 1.0;
+        }
+      `
     }
+
+
+
   });
-  const blue = 0.5;
+  const [time, setTime] = useState(0.0);
+
+  useEffect(() => {
+    console.log('MAJ time');
+
+    const interval = setInterval(() => {
+      setTime(time => time + 0.1);
+    }
+    , 100);
+    return () => clearInterval(interval);
+    // eslint-disable-next-line
+  }, []);
+
   return (
   <IonPage id="map-view">
     <IonHeader>
@@ -46,13 +110,22 @@ const MapView: React.FC<MapViewProps> = ({ locations, mapCenter }) => {
 
     <IonContent class="map-page">
       <p>MapView</p>
-      <Surface width={100} height={100} >
+      <Surface width={200} height={200} >
         <Node
-          shader={shaders.helloBlue}
-
+          shader={shaders.smoke}
+          uniforms={{
+            shift: 1.6,
+            time: time,
+            speed: [1.0, 1.0],
+            resolution: [200, 200]
+          }}
         />
       </Surface>
- {/* onMouseMove={this.handleMouseMove}uniforms={{
+ {/*
+ ok <Node
+          shader={shaders.helloBlue}
+        />
+ onMouseMove={this.handleMouseMove}uniforms={{
             shift: 1.6,
             time: time / 1000,
             speed: [1.0, 1.0],
